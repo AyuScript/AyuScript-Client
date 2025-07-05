8be0bb75301ced5e02e24ceb683d87943a92e94f
LittleSwift <60036604+LittleSwift@users.noreply.github.com>
2025-07-06 02:10:05 +0800

    build: support for '@' based path

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import monkey from 'vite-plugin-monkey';
 import { version } from "./package.json";
+import * as path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    minify: true
+    minify: true,
+  },
+  resolve: {
+    alias:  {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   plugins: [
     vue(),
